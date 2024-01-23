d1fd3e884604534a6963969d7db0325bf3741f85
Juahjoah <122463317+Juahjoah@users.noreply.github.com>
2024-01-24 02:18:52 +0900

    Time: 66 ms (46.80%), Space: 50.1 MB (5.11%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -3,18 +3,23 @@
  * @return {Function}
  */
 var compose = function(functions) {
-    
+  if (functions.length === 0) {
     return function(x) {
-        if(functions.legnth===0) 
-          return x
-          for(let i = functions.length-1; i>=0; i--) {
-            x = functions[i](x)
-          }
-          return x            
-            
-    } 
-
+      return x
+    }
+  } else {
+    return function(x) {
+      let answer = x;
+      for (let i = functions.length - 1; i >= 0; i--) {
+        answer = functions[i](answer)
+      }
+      return answer;     
+    }
+  }
 };
+
+
+
 
 /**
  * const fn = compose([x => x + 1, x => 2 * x])
