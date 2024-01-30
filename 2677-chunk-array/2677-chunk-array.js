2541151c6693b097e12afc83d2f9854b877c3b02
Juahjoah <122463317+Juahjoah@users.noreply.github.com>
2024-01-30 21:14:01 +0900

    Time: 54 ms (68.46%), Space: 52.1 MB (6.10%) - LeetHub

--- 2677-chunk-array/2677-chunk-array.js
+++ 2677-chunk-array/2677-chunk-array.js
@@ -3,17 +3,38 @@
  * @param {number} size
  * @return {Array}
  */
-    
-var chunk = function(arr, size) {
-  let answer = [];
-  let index = 0;
-  
-    while (index < arr.length) {
-      answer.push(arr.slice(index, size + index))
-      index += size
+
+var chunk = function (arr, size) {
+  if (!arr.length) return [];
+  if (size >= arr.length) return [arr];
+  const answer = [];
+
+  let tempArr = [];
+  arr.forEach((item, idx) => {
+    tempArr.push(item);
+    if ((idx + 1) % size === 0) {
+      answer.push([...tempArr]);
+      tempArr = [];
     }
-  return answer  
+  });
+
+  if (tempArr.length) answer.push(tempArr);
+
+  return answer;
 };
+
+
+    
+// var chunk = function(arr, size) {
+//   let answer = [];
+//   let index = 0;
+  
+//     while (index < arr.length) {
+//       answer.push(arr.slice(index, size + index))
+//       index += size
+//     }
+//   return answer  
+// };
 
 
 
