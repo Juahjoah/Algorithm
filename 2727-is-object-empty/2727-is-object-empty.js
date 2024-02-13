095ed93e95e1649934999f8141a98861a868d021
Juahjoah <122463317+Juahjoah@users.noreply.github.com>
2024-02-13 22:57:36 +0900

    Time: 39 ms (98.48%), Space: 50.1 MB (5.53%) - LeetHub

---
+++ 2727-is-object-empty/2727-is-object-empty.js
@@ -0,0 +1,12 @@
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+  return Object.keys(obj).length === 0;  
+    
+  // if (!obj) {
+  //   return true
+  // }
+  //   return false
+};
