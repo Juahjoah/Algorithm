007b7ec9f2448b6772def93c59a0a08d2cbefb66
Juahjoah <122463317+Juahjoah@users.noreply.github.com>
2024-10-26 22:08:09 +0900

    [Silver IV] Title: 수 찾기, Time: 700 ms, Memory: 46720 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,28 @@
+N = int(input())
+A = list(map(int, input().split()))
+A.sort()
+
+M = int(input())
+input_list = list(map(int, input().split()))
+
+
+for m in input_list:
+    left = 0
+    right = N - 1
+
+    while left <= right:
+        mid = (left+right) // 2
+
+        if m > A[mid]:
+            left = mid + 1
+        elif m < A[mid]:
+            right = mid - 1
+        else:
+            left = mid
+            right = mid
+            break
+
+    if left == mid and right == mid:
+        print(1)
+    else:
+        print(0)
