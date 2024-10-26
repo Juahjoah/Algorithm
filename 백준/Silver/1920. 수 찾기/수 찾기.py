e9c275c09ed85d5700afc099d94dbca9c4b2e815
Juahjoah <122463317+Juahjoah@users.noreply.github.com>
2024-10-26 22:51:44 +0900

    [Silver IV] Title: 수 찾기, Time: 160 ms, Memory: 50804 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,38 +1,16 @@
-
-
-def binary_search(left, right):
-
-    while left <= right:
-        mid = (left+right) // 2
-
-        if m > A[mid]:
-            left = mid + 1
-        elif m < A[mid]:
-            right = mid - 1
-        else:
-            left = mid
-            right = mid
-            break
-
-    if left == mid and right == mid:
-        print(1)
-    else:
-        print(0)
-
-
-
-
-
 N = int(input())
 A = list(map(int, input().split()))
-A.sort()
+B = set()
+
+for i in A:
+    B.add(i)
 
 M = int(input())
 input_list = list(map(int, input().split()))
 
 
-for m in input_list:
-    left = 0
-    right = N - 1
-
-    binary_search(left, right)
+for m in input_list:    
+    if m in B:
+        print(1)
+    else:
+        print(0)
