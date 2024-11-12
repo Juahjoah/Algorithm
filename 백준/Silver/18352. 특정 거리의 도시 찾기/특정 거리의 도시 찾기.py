86158ff1f4ca50a6ea36592481082cf2a5390a06
Juahjoah <122463317+Juahjoah@users.noreply.github.com>
2024-11-12 16:01:11 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1016 ms, Memory: 154600 KB -BaekjoonHub

---
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -0,0 +1,42 @@
+from collections import deque
+
+def BFS(K, X):
+    # 최단 거리 탐색
+    queue = deque()
+    queue.append(X)  # 시작점
+    visited[X] = 1
+    cnt = 0
+
+    while queue:
+        if cnt == K:
+            # return start
+            break
+        else:
+            cnt += 1
+
+        for i in range(len(queue)):
+            start = queue.popleft()
+
+            for next_city in tree_list[start]:
+                if visited[next_city] == 0:
+                    visited[next_city] = 1
+                    queue.append(next_city)
+
+    answer = list(queue)
+    if not answer:
+        answer.append(-1)
+    return sorted(answer)
+
+N, M, K, X = map(int, input().split())
+# input_list = [list(map(int, input().split())) for _ in range(M)]
+
+# 이동 여부를 기록할 배열
+# visited = [[0]*(M) for _ in range(M)]
+visited = [0] * (N+1)       # 인덱스 값을 맞추기 위해 N+1로 구현
+tree_list = [[] for _ in range(N+1)]
+
+for i in range(M):
+    A, B = map(int, input().split())
+    tree_list[A].append(B)
+
+print(*BFS(K, X), sep="\n")
