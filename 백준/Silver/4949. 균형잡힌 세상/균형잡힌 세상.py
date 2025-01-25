34ccf408daaeb01199bf6b8ead81deecf0a65375
Juahjoah <122463317+Juahjoah@users.noreply.github.com>
2025-01-25 16:12:19 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 220 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -1,38 +1,33 @@
 
-
-def checkBalanced(inputText):
+def vps(sentence):
     stack = []
 
-    # 각 문장별로 비교하기
-    for i in inputText:
-        # 여는 괄호일 경우, 담기
-        if i == "(" or i == "[":
-            stack.append(i)
-
-        # 닫는 괄호가 나왔을 경우, 꺼내서 비교하기
-        elif i == ")" or i == "]":
-            # stack이 비어있다면 짝이 맞지 않음
+    for inp in sentence:
+        if inp == '(' or inp == '[':
+            stack.append(inp)
+        elif inp == ')':
             if not stack:
-                return "no"
-            top = stack.pop()
-            if i == ")" and top != "(":
-                return "no"
-            elif i == "]" and top != "[":
-                return "no"
+                return 'no'
+            elif stack[-1] == '(':
+                stack.pop()
+            else:
+                return 'no'
 
-    # stack에 무언가 남아있다면, 짝이 맞지 않은 것
-    if stack :
-        return "no"
+        elif inp == ']':
+            if not stack:
+                return 'no'
+            elif stack[-1] == '[':
+                stack.pop()
+            else:
+                return 'no'
 
-    # 해당하지 않으면 짝이 맞음.
-    return "yes"
-
+    if stack:
+        return 'no'
+    return 'yes'
 
 while True:
-    # inputText = input().strip()
-    inputText = input().rstrip()
-    # 맨 마지막 줄에 온점이 들어오는 경우 끝남.
-    if inputText == ".":
+    sentence = list(input())
+    if sentence == ['.']:
         break
-    print(checkBalanced(inputText))
 
+    print(vps(sentence))
