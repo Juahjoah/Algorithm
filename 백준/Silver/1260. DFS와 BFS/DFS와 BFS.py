bca335f9dab7597ea7b36621b303b27a122dd4bc
Juahjoah <122463317+Juahjoah@users.noreply.github.com>
2025-04-22 23:33:07 +0900

    [Silver II] Title: DFS와 BFS, Time: 316 ms, Memory: 36684 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,43 +1,59 @@
-import sys
+# 1260. DFS와 BFS
+from collections import deque
 
-# inputNum = list(map(int, sys.stdin.read().split()))
 
-# input 받기
+def dfs(node):
+    # 1. 스택 활용
+    # 2. 재귀 활용
+
+    # 해당 깊이의 노드를 방문 처리
+    visited_dfs[node] = True
+    answer_dfs.append(node)
+
+    # 그 노드에 대한 for문
+    for n in adj_list[node]:
+        # 방문하지 않았다면, 해당 노드로 dfs 시작
+        if not visited_dfs[n]:
+            dfs(n)
+
+
+def bfs():
+    queue = deque()
+    queue.append(V)
+    visited_bfs[V] = True
+
+    while queue:
+        node = queue.popleft()
+
+        answer_bfs.append(node)
+
+        for n in adj_list[node]:
+            if not visited_bfs[n]:
+                queue.append(n)
+                visited_bfs[n] = True
+
+
+
 N, M, V = map(int, input().split())
-dataTree = [[0]*(N+1) for _ in range(N+1)]
+input_list = [list(map(int, input().split())) for _ in range(M)]
 
-# 노드트리를 표현할 행렬 만들기
-for i in range(M):
-    a, b = map(int, input().split())
-    dataTree[a][b] = dataTree[b][a] = 1
+adj_list = [[] for _ in range(N + 1)]
 
-# 방문 여부 확인하기
-visitedDFS = [0]*(N+1)
-visitedBFS = [0]*(N+1)
+for x, y in input_list:
+    adj_list[x].append(y)
+    adj_list[y].append(x)
 
-# DFS 구현
-def DFS(V):
-    visitedDFS[V] = 1                                    # 방문처리
-    print(V, end=' ')                                    # 방문한 노드는 하나씩 출력
-    for i in range(1, N+1):
-        if dataTree[V][i] == 1 and visitedDFS[i] == 0 :  # 연결되어 있고, 방문하지 않은 노드라면 출력
-            DFS(i)
+for a in adj_list:
+    a.sort()
 
+visited_dfs = [False] * (N + 1)
+visited_bfs = [False] * (N + 1)
 
-# BFS 구현
-def BFS(V):
-    queue = [V]                                          # 시작점 담기
-    visitedBFS[V] = 1                                    # 방문처리
-    while queue :
-        V = queue.pop(0)
-        print(V, end=' ')
-        for j in range(1, N+1):
-            if (dataTree[V][j] == 1 and visitedBFS[j] == 0) :
-                queue.append(j)
-                visitedBFS[j] = 1                        # 방문처리
+answer_dfs = []
+answer_bfs = []
 
+dfs(V)
+bfs()
 
-# 함수 출력
-DFS(V)
-print()
-BFS(V)
+print(*answer_dfs)
+print(*answer_bfs)
