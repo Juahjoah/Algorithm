2e44e3888e3b9afcb8ee719f5cc7971a1b355ee4
Juahjoah <122463317+Juahjoah@users.noreply.github.com>
2025-04-23 22:56:43 +0900

    [Silver I] Title: 숨바꼭질, Time: 96 ms, Memory: 36316 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,30 +1,34 @@
+# 1697. 숨바꼭질
+import sys
 from collections import deque
 
-def BFS(N, K):
+input = sys.stdin.readline
+
+def bfs():
     queue = deque()
+    queue.append((N, 0))
+    visited[N] = True
 
-    visited = [0] * 100001
-
-    # 수빈이의 위치를 추가
-    queue.append(N)
-    
     while queue:
-        current_location = queue.popleft()
+        loc, time = queue.popleft()
 
-        # 동생의 위치가 되었다면, 함수를 종료
-        if current_location == K:
-            return visited[current_location]
+        if loc == K:
+            return time
 
-        # 이동할 수 있는 위치 : 할 수 있는 행위는 걷거나, 순간이동하거나
-        next_location = [current_location - 1, current_location + 1, current_location * 2]
-
-        for location in next_location:
-            # if 0 <= location < 100001 and not visited[location] != True:
-            if 0 <= location < 100001 and visited[location] == 0:
-                queue.append(location)
-                
-                visited[location] = visited[current_location] + 1
+        # x-1로 이동, x+1로 이동, *2 이동
+        if 0 <= loc*2 <= 100000 and not visited[loc*2]:
+            visited[loc*2] = True
+            queue.append((loc * 2, time + 1))
+        if 0 <= loc - 1 <= 100000 and not visited[loc - 1]:
+            visited[loc - 1] = True
+            queue.append((loc - 1, time + 1))
+        if 0 <= loc + 1 <= 100000 and not visited[loc + 1]:
+            visited[loc + 1] = True
+            queue.append((loc + 1, time + 1))
 
 N, K = map(int, input().split())
 
-print(BFS(N, K))
+visited = [False] * 100001
+answer = bfs()
+
+print(answer)
