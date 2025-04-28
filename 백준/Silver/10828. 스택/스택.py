f61b174fe984e0b22bd4fee8b27d90f6787e468d
Juahjoah <122463317+Juahjoah@users.noreply.github.com>
2025-04-28 22:48:56 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,39 @@
+# 10828. 스택
+
+'''
+push X: 정수 X를 스택에 넣는 연산이다.
+pop: 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+size: 스택에 들어있는 정수의 개수를 출력한다.
+empty: 스택이 비어있으면 1, 아니면 0을 출력한다.
+top: 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+'''
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+stack = []
+
+for _ in range(N):
+    order = input().split()
+
+    if order[0] == 'push':
+        stack.append(order[1])
+    elif order[0] == 'pop':
+        if stack:
+            x = stack.pop()
+            print(x)
+        else:
+            print(-1)
+    elif order[0] == 'size':
+        print(len(stack))
+    elif order[0] == 'empty':
+        if stack:
+            print(0)
+        else:
+            print(1)
+    elif order[0] == 'top':
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
