a9beeb0d2b51e062f86b7285254b2183c402e032
Juahjoah <122463317+Juahjoah@users.noreply.github.com>
2025-05-09 03:16:39 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.66 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    var answer = [[]];
+    for (let i = 0; i < n; i++) {
+        answer[i] = [];
+        
+        for (let j = 0; j < n; j++) {
+            if (i === j) {
+                answer[i].push(1)
+            } else {
+                answer[i].push(0)
+            }
+        }
+    }
+    return answer;
+}
