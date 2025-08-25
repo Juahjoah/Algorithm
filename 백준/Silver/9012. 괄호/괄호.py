a8c86a6e28bbb61ea0eda6c1dca79afbced6a99f
Juahjoah <122463317+Juahjoah@users.noreply.github.com>
2025-08-26 01:22:53 +0900

    [Silver IV] Title: 괄호, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,21 +1,29 @@
+# 괄호
 
-def vps(inp):
-    stack = []
-
-    for i in inp:
-        if i == "(":
-            stack.append(i)
-        elif i == ")":
-            if len(stack) == 0:
-                return "NO"
-            stack.pop()
-
-    if stack:
-        return "NO"
-    return "YES"
+import sys
+input = sys.stdin.readline
 
 N = int(input())
-for _ in range(N):
-    sentence = list(input())
-    print(vps(sentence))
 
+for _ in range(N):
+    input_text = list(input().strip())
+    stack = []
+
+    for i in range(len(input_text)):
+        if input_text[i] == '(':
+            stack.append(input_text[i])
+        else:
+            if stack:
+                if stack[-1] == '(':
+                    stack.pop()
+                else:
+                    stack.append(')')
+                    break
+            else:
+                stack.append(')')
+                break
+
+    if stack:
+        print("NO")
+    else:
+        print("YES")
