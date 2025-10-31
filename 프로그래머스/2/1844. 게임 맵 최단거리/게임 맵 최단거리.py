f8c587b8eecbb061fe836b94cf0bafd573a14e7c
Juahjoah <122463317+Juahjoah@users.noreply.github.com>
2025-10-31 22:49:55 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.51 ms, Memory: 9.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,38 +1,28 @@
 from collections import deque
 
-dx = [1, 0, -1, 0]
-dy = [0, -1, 0, 1]
-
-
-def BFS(maps):
-    M = len(maps[0])
-    N = len(maps)
+def solution(maps):
+    answer = -1
+    n = len(maps[0])
+    m = len(maps)
     
-    visited = [[0]*M for _ in range(N)]
+    dx = [0, 1, 0, -1]
+    dy = [1, 0, -1, 0]
     
-    queue = deque()
-    queue.append((0, 0))
+    visited = [[0] * n for _ in range(m)]
+    queue = deque([(0, 0, 1)])
     visited[0][0] = 1
-    # move = 1
     
     while queue:
-        x, y = queue.popleft()
+        x, y, cnt = queue.popleft()
         
-        if x == N - 1 and y == M - 1:
-            return visited[x][y]
+        if x == m - 1 and y == n - 1:
+            return cnt      
         
         for d in range(4):
             nx, ny = x + dx[d], y + dy[d]
             
-            if 0 <= nx < N and 0 <= ny < M and visited[nx][ny] == 0 and maps[nx][ny] == 1:
-                queue.append((nx, ny))
-                visited[nx][ny] = visited[x][y] + 1
-                # move += 1
+            if 0 <= nx < m and 0 <= ny < n and visited[nx][ny] == 0 and maps[nx][ny] == 1:
+                visited[nx][ny] = 1
+                queue.append((nx, ny, cnt + 1))
                 
-    return -1
-    
-
-def solution(maps):    
-    # answer = BFS(maps)
-    
-    return BFS(maps)
+    return answer
